663dc2a4ce9e3c7ad1337ce69ee2535f0cae95cc
shrfrn <shrfrn@gmail.com>
2024-05-30 12:12:39 +0300

    Add local storage support

--- js/services/todo.service.js
+++ js/services/todo.service.js
@@ -28,32 +28,49 @@ function getTodoById(todoId) {  // Read
 }
 
 function addTodo(txt) {         // Create
-
     const todo = _createTodo(txt)
     gTodos.unshift(todo)
+
+    _saveTodos()
 }
 
 function removeTodo(todoId) {   // Delete
     const idx = gTodos.findIndex(todo => todo.id === todoId)
     gTodos.splice(idx, 1)
+
+    _saveTodos()
 }
 
 function toggleTodo(todoId) {   // Update
     const todo = gTodos.find(todo => todo.id === todoId)
     todo.isDone = !todo.isDone
+
+    _saveTodos()
 }
 
 function _createTodos() {
+    gTodos = loadFromStorage('todos')
+    if(gTodos && gTodos.length !== 0) return
+
+    // Never load app without ant data
+    // Create demo data if there is no data in storage
+    
     gTodos = [
         _createTodo('Do This'),
         _createTodo('Do That'),
         _createTodo('Try This'),
     ]
+    _saveTodos()
 }
+
 function _createTodo(txt) {
     return {
         id: makeId(),
         txt,
         isDone: false,
     }
+}
+
+function _saveTodos() {
+    saveToStorage('todos', gTodos)
 }
